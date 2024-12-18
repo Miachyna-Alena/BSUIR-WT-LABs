b25cf492d413d533be6234546348a9fb68e97a51
Alena Miachyna <vysotskayaalena5@gmail.com>
2024-12-18 15:27:23 +0300

    Add LAB 2 files.

--- Miachyna.UI/Controllers/HomeController.cs
+++ Miachyna.UI/Controllers/HomeController.cs
@@ -1,12 +1,36 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace Miachyna.UI.Controllers
 {
     public class HomeController : Controller
     {
+
+        private readonly ILogger<HomeController> _logger;
+        private readonly List<ListDemo> _listData;
+
+        public class ListDemo
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+        }
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+            _listData = new List<ListDemo>
+            {
+            new ListDemo {Id=1, Name="Item 1"},
+            new ListDemo {Id=2, Name="Item 2"},
+            new ListDemo {Id=3, Name="Item 3"}
+            };
+        }
+
         public IActionResult Index()
         {
-            return View();
+            ViewData["LabTitle"] = "Laboratory work 02";
+            SelectList data = new SelectList(_listData, "Id", "Name");
+            return View(data);
         }
     }
 }
