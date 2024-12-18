2ac06517f37265fdddec2455187546ab3b98cc3b
Alena Miachyna <vysotskayaalena5@gmail.com>
2024-12-18 13:17:42 +0300

    Add project files.

---
+++ Miachyna.UI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Miachyna.UI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
